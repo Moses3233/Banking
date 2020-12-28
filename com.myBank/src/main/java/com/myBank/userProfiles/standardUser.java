b541af01b42b0e99fd609c94b3711d55cdee01d2
Moise Jerome <moisejerome@Moises-Air.attlocal.net>
2020-12-28 10:05:57 -0500

    starting

---
+++ com.myBank/src/main/java/com/myBank/userProfiles/standardUser.java
@@ -0,0 +1,13 @@
+package com.myBank.userProfiles;
+
+public class standardUser {
+String username, password, firstName, lastName,email;
+
+
+public standardUser() {}
+
+public void withdrawFunds(int amount) {}
+public void depositFunds(int amount) {}
+public void transferFunds(standardUser recipient, int amount) {}
+
+}
