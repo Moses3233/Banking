6f162ec05d310304d469a32e6ba7c7cd0e281c33
Moise <mjero002@fiu.edu>
2021-01-06 14:54:56 -0500

    We got some models

--- BankingPro/myBankApp/src/main/java/myBankApp/model/accounts.java
+++ BankingPro/myBankApp/src/main/java/myBankApp/model/accounts.java
@@ -2,4 +2,64 @@ package myBankApp.model;
 
 public class accounts {
 
+
+	private int acctnum;
+	private String username;
+	private String type;
+	private double balance;
+	private String status;
+	
+	public accounts() {
+		
+		
+	}
+
+	public int getAcctnum() {
+		return acctnum;
+	}
+
+	public void setAcctnum(int acctnum) {
+		this.acctnum = acctnum;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	@Override
+	public String toString() {
+		
+	return "Account [Account Number: " + acctnum +", Username: " + username + ", Account Type: " + type +  ", Balance: " + balance +", Status: " +status+" ]";
+
+	}
+	
+	
 }
--- BankingPro/myBankApp/src/main/java/myBankApp/model/users.java
+++ BankingPro/myBankApp/src/main/java/myBankApp/model/users.java
@@ -1,5 +1,107 @@
 package myBankApp.model;
 
 public class users {
+private String fname;
+private String lname;
+private String username;
+private String password;
+private String role;
+private int age;
+private String gender;
+private String email;
+private String address;
+private String zip;
+private String city;
+private String state;
+private String country;
+
+public String getFname() {
+	return fname;
+}
+public void setFname(String fname) {
+	this.fname = fname;
+}
+public String getLname() {
+	return lname;
+}
+public void setLname(String lname) {
+	this.lname = lname;
+}
+public String getUsername() {
+	return username;
+}
+public void setUsername(String username) {
+	this.username = username;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+public String getRole() {
+	return role;
+}
+public void setRole(String role) {
+	this.role = role;
+}
+public int getAge() {
+	return age;
+}
+public void setAge(int age) {
+	this.age = age;
+}
+public String getGender() {
+	return gender;
+}
+public void setGender(String gender) {
+	this.gender = gender;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getCity() {
+	return city;
+}
+public void setCity(String city) {
+	this.city = city;
+}
+public String getAddress() {
+	return address;
+}
+public void setAddress(String address) {
+	this.address = address;
+}
+public String getZip() {
+	return zip;
+}
+public void setZip(String zip) {
+	this.zip = zip;
+}
+public String getState() {
+	return state;
+}
+public void setState(String state) {
+	this.state = state;
+}
+public String getCountry() {
+	return country;
+}
+public void setCountry(String country) {
+	this.country = country;
+}
+
+@Override
+public String toString() {
+	
+return "User [ Role: " + role + ", Username: " + username +", Password: " + password + 
+			", First Name: " + fname + ", Last Name: " +lname+ ", Email: " + email +
+			", Age: " + age + ", Gender: " +gender+ ", Address: " + address +
+			", City: " + city + ", State: " +state+ ", ZIP: " + zip +", Country: " + country +" ]";
+
+}
 
 }
